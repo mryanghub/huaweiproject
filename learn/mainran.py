e68ff2580cac09abcebdd299d7f88326197c9349
mryanghub <1378055043@qq.com>
2020-12-01 23:08:39 +0800

    m

--- learn/mainran.py
+++ learn/mainran.py
@@ -1,11 +1,11 @@
 import sys
-import mainHD
+import testnew
 from PyQt5.QtWidgets import QApplication,QMainWindow
 
 if __name__ == '__main__':
     app=QApplication(sys.argv)
     mainWindow=QMainWindow()
-    ui=mainHD.Ui_MainWindow()
+    ui=testnew.Ui_MainWindow()
     ui.setupUi(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
